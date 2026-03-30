e503b5f4e7679965fe436c97b5511d83c0bf0043
Sabiur Rahman <227128355+rahmansabiurfx@users.noreply.github.com>
2026-03-30 19:29:08 +0530

    Changed version to 0.2.0

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ from contextlib import asynccontextmanager
 # ──────────────────────────────────────────────
 
 APP_NAME = os.getenv("APP_NAME", "orderly-app")
-APP_VERSION = os.getenv("APP_VERSION", "0.1.0")
+APP_VERSION = os.getenv("APP_VERSION", "0.2.0")
 ENVIRONMENT = os.getenv("ENVIRONMENT", "local")
 
 @asynccontextmanager
